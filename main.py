710d4a52dfe9aab9711bdb7119b75288ad895344
Pelotrio <45769595+Pelotrio@users.noreply.github.com>
2022-03-13 16:42:28 +0100

    Added functions to jpegify the image and template.png

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-def main():
-    print("Hello World!")
+from PIL import Image, ImageEnhance
+import io
 
 
-if __name__ == "__main__":
-    main()
+def jpegify(image, power=92, repetitions=50):
+    out = image.convert("RGB")
+    for i in range(repetitions):
+        buffer = io.BytesIO()
+        out.save(buffer, "jpeg", quality=100 - power)
+        buffer.seek(0)
+        out = Image.open(buffer)
+    return out
+
+
+def apply_color(template, image, template_to_grayscale=True):
+    template = template.convert("RGB")
+    image = image
+
+    if template_to_grayscale:
+        template = template.convert("L").convert("RGB")
+
+    image.putalpha(ImageEnhance.Brightness(image.split()[3]).enhance(0.5))
+    return Image.composite(image, template, image)
