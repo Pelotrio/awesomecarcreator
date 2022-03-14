2f68cfb330eea9be57e0bb510720fa5a92729152
vshab <v.shabanov@hotmail.com>
2022-03-14 22:03:30 +0400

    renamed functions and cleanup

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def generate_video(image, audio_filename, output_filename, duration = 10, seek_t
     #out file stuff
     command += f"-r {framerate} -vf \"scale=720:720\" -b:v {video_bitrate} -fs {max_filesize} -t {duration} -movflags +faststart {output_filename}"
 
-    out, err = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate(input = image_buffer.read())
+    _, err = subprocess.Popen(command, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE).communicate(input = image_buffer.read())
 
     if err != b"":
         raise Exception(f"Unknown error, stderr: {err}")
@@ -40,21 +40,21 @@ def jpegify(image, power = 92, repetitions = 50):
     return out
 
 def apply_color_overlay(image, overlay, grayscale = True, alpha = 0.5):
-    if grayscale:
-        image = image.convert("L").convert("RGB")
-    else:
-        image = image.convert("RGB")
-
     overlay = overlay.convert("RGBA")
+    
+    if grayscale:
+        image = image.convert("L")
+
+    image = image.convert("RGB")
     
     overlay.putalpha(ImageEnhance.Brightness(overlay.split()[3]).enhance(alpha))
     return Image.composite(overlay, image, overlay)
 
-def apply_watermark(image, watermark, pos):
+def put_watermark(image, watermark, pos):
     image.paste(watermark, pos, watermark)
     return image
 
-def apply_logo(image, logo, pos, size, angle = 0):
+def put_logo(image, logo, pos, size, angle = 0):
     logo = logo.resize(size)
     logo = logo.rotate(-angle, expand = True)
     #TODO: more magic
@@ -65,15 +65,15 @@ def change_color(image, color):
     image = image.convert("RGB")
     image_array = numpy.array(numpy.asarray(image).astype('float'))
 
-    image_r, image_g, image_b = numpy.rollaxis(image_array, axis = -1)
-    image_h, image_s, image_v = numpy.vectorize(colorsys.rgb_to_hsv)(image_r, image_g, image_b)
+    image_rgb = numpy.rollaxis(image_array, axis = -1)
+    image_h, image_s, image_v = numpy.vectorize(colorsys.rgb_to_hsv)(*image_rgb)
 
-    color_h, color_s, color_v = colorsys.rgb_to_hsv(color[0], color[1], color[2])
+    color_h, _, _ = colorsys.rgb_to_hsv(color[0], color[1], color[2])
 
     image_h = color_h
-    image_r, image_g, image_b = numpy.vectorize(colorsys.hsv_to_rgb)(image_h, image_s, image_v)
+    image_rgb = numpy.vectorize(colorsys.hsv_to_rgb)(image_h, image_s, image_v)
 
-    out_array = numpy.dstack((image_r, image_g, image_b))
+    out_array = numpy.dstack(image_rgb)
 
     return Image.fromarray(out_array.astype("uint8"), "RGB")
 
@@ -84,18 +84,18 @@ def colorize_with_image(image, overlay):
     image_array = numpy.array(numpy.asarray(image).astype('float'))
     overlay_array = numpy.array(numpy.asarray(overlay).astype('float'))
 
-    image_r, image_g, image_b = numpy.rollaxis(image_array, axis = -1)
-    image_h, image_s, image_v = numpy.vectorize(colorsys.rgb_to_hsv)(image_r, image_g, image_b)
+    image_rgb = numpy.rollaxis(image_array, axis = -1)
+    image_h, image_s, image_v = numpy.vectorize(colorsys.rgb_to_hsv)(*image_rgb)
 
-    overlay_r, overlay_g, overlay_b = numpy.rollaxis(overlay_array, axis = -1)
-    overlay_h, overlay_s, overlay_v = numpy.vectorize(colorsys.rgb_to_hsv)(overlay_r, overlay_g, overlay_b)
+    overlay_rgb = numpy.rollaxis(overlay_array, axis = -1)
+    overlay_h, overlay_s, _ = numpy.vectorize(colorsys.rgb_to_hsv)(*overlay_rgb)
 
     image_h = overlay_h
     image_s = overlay_s * image_s
 
-    image_r, image_g, image_b = numpy.vectorize(colorsys.hsv_to_rgb)(image_h, image_s, image_v)
+    image_rgb = numpy.vectorize(colorsys.hsv_to_rgb)(image_h, image_s, image_v)
 
-    out_array = numpy.dstack((image_r, image_g, image_b))
+    out_array = numpy.dstack(image_rgb)
 
     return Image.fromarray(out_array.astype("uint8"), "RGB")
 
@@ -103,7 +103,6 @@ def ruin_resolution(image, power = 3):
     return image.resize((int(image.size[0] / 2**power), int(image.size[1] / 2**power))).resize((image.size[0], image.size[1]))
 
 
-#everything is subject to change
 
 template_name = "template"
 
